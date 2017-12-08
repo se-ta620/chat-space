08f806383af0c48c16d2bc03d8a7db2ab1576ede
se-ta620 <skawsk620@gmail.com>
2017-12-08 18:57:08 +0900

    フラッシュメッセージの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
