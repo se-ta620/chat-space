25614173b4188191a39f8822d3ffcdee0a923653
se-ta620 <skawsk620@gmail.com>
2017-12-10 17:04:18 +0900

    グループ作成機能の追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,11 +6,11 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-      if @group.save
-        redirect_to root_path, notice: "グループを作成しました"
-      else
-        render :new
-      end
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def update
