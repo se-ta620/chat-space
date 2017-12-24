666dcd8b22b8442ede4524bdc449b9d91e8c0781
se-ta620 <skawsk620@gmail.com>
2017-12-25 08:17:12 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,65 +1,73 @@
-  $(function(){
-    function buildHTML(message){
-      var html = `<div class= contents__main__middle__message-content>
-                    <div class= contents__main__middle__message-content__over>
-                      <div class= contents__main__middle__message-content__over__user-name> ${message.name}</div>
-                      <div class= contents__main__middle__message-content__over__post-time> ${message.created_at}</div>
+$(function(){
+  function buildHTML(message){
+    var html = `<div class= contents__main__middle__message-content data-message-id = ${message.id}>
+                  <div class= contents__main__middle__message-content__over>
+                    <div class= contents__main__middle__message-content__over__user-name>
+                      ${message.name}
                     </div>
-                    <div class= contents__main__middle__message-content__under>
-                      <div class= ccontents__main__middle__message-content__under__message> ${message.body} </div>`;
-          html += (message.image) ?
-                    (`<div class= contents__main__middle__message-content__under__image> <img src= "${message.image}"></div>`):
-                  ('</div>');
+                    <div class= contents__main__middle__message-content__over__post-time>
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class= contents__main__middle__message-content__under>
+                    <div class= contents__main__middle__message-content__under__message>
+                      ${message.body}
+                    </div>
+                    <div class= contents__main__middle__message-content__under__image>`
+        html += (message.image) ?
+                      (`<img src= "${message.image}">
+                    </div>`):
                 ('</div>');
-      return html;
-    }
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var href = $(this).attr('action');
-      $.ajax({
-        url: href,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.contents__main__middle').append(html);
-        $('.contents__main__bottom__form__text-field').val('');
-        $('.contents__main__bottom__form__picture-field-label').val('');
-        $('.contents__main__middle').animate({scrollTop: $('.contents__main__middle')[0].scrollHeight}, 'slow');
-      })
-      .fail(function(){
-        alert('error');
-      });
-      return false;
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = $(this).attr('action');
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.contents__main__middle').append(html);
+      $('.contents__main__bottom__form__text-field').val('');
+      $('.contents__main__bottom__form__picture-field-label').val('');
+      $('.contents__main__middle').animate({scrollTop: $('.contents__main__middle')[0].scrollHeight}, 'slow');
+    })
+    .fail(function(){
+      alert('error');
     });
+    return false;
   });
-
   // メッセージ自動更新機能
   var interval = setInterval(function() {
+    var id = $('.contents__main__middle__message-content').last().data("message-id");
     if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-  $.ajax({
-    url: location.href,
-    type: "GET",
-    dataType: 'json'
-  })
-  .done(function(data) {
-    var id = $('.contents__main__middle__message-content').data('messageId');
-    var insertHTML = '';
-    data.messages.forEach(function(message) {
-      if (message.id > id ) {
-        insertHTML += buildHTML(message);
-      }
+      $.ajax({
+        url: location.href,
+        type: "GET",
+        dataType: 'json',
+        data: { id: id }
+      })
+    .done(function(data) {
+      data.messages.forEach(function(message) {
+        if (message.id > id ) {
+        var html = buildHTML(message)
+        $('.contents__main__middle').append(html);
+        $('.contents__main__middle').animate({scrollTop: $('.contents__main__middle')[0].scrollHeight}, 'slow');
+        }
+      });
+    })
+    .fail(function(data) {
+      alert('自動更新に失敗しました');
     });
-    $('.contents__main__middle__message-content').prepend(insertHTML);
-  })
-  .fail(function(data) {
-    alert('自動更新に失敗しました');
-  });
-} else {
-  clearInterval(interval);
- }} , 5 * 1000 );
+      } else {
+      clearInterval(interval);
+    }
+  } , 5 * 1000 );
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,11 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @currentgroup = Group.find(params[:group_id])
-    @messages = @currentgroup.messages.order(created_at: :DESC).includes(:user)
-    @message = current_user.messages.new
+    @messages = @currentgroup.messages.includes(:user)
     respond_to do |format|
       format.json
       format.html
+    @message = current_user.messages.new
     end
   end
 
